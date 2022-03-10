f93da8af1de69d2997c28c90a2c3750aee876f53
Said van de Klundert <said.van.de.klundert@gmail.com>
2022-03-10 21:25:54 +0100

    adding stuff

--- src/setup.py
+++ src/setup.py
@@ -4,13 +4,13 @@ setup(
     name="ezaws",
     version="0.1",
     packages=["ezaws"],
-    description="This is an example package that can serve as a template.",
-    url="https://github.com/saidvandeklundert/python_template",
+    description="An easy to use interface to AWS, ezaws.",
+    url="https://github.com/saidvandeklundert/ezaws",
     classifiers=["Programming Language :: Python :: 3"],
     python_requires=">=3.10, <4",
     project_urls={
-        "Bug Reports": "https://github.com/saidvandeklundert/python_template/issues",
-        "Source": "https://github.com/saidvandeklundert/python_template",
-        "Documentation": "https://github.com/saidvandeklundert/python_template/wiki",
+        "Bug Reports": "https://github.com/saidvandeklundert/ezaws/issues",
+        "Source": "https://github.com/saidvandeklundert/ezaws",
+        "Documentation": "https://github.com/saidvandeklundert/ezaws",
     },
 )
