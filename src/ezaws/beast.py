1742f3e8dccd25ad5f5a5e6efc8f675ac4eaf738
Said van de Klundert <said.van.de.klundert@gmail.com>
2022-03-10 21:42:49 +0100

    remove old files

--- src/ezaws/beast.py
+++
@@ -1,6 +0,0 @@
-class Beast:
-    def __init__(self, name: str):
-        self.name = name
-
-    def say_something(self) -> str:
-        return "I am a beast"
--- src/ezaws/human.py
+++
@@ -1,6 +0,0 @@
-class Human:
-    def __init__(self, name: str):
-        self.name = name
-
-    def say_something(self) -> str:
-        return "I made a template repo"
