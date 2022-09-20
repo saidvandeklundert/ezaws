8af592ff6d4604d62e2a812ac0c0b6e7730791ad
Said van de Klundert <said.van.de.klundert@gmail.com>
2022-09-20 09:14:54 +0200

    change setup

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from pathlib import Path
 
 this_directory = Path(__file__).parent
@@ -6,9 +6,9 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="ezaws",
-    version="0.23",
+    version="0.25",
     package_dir={"ezaws": "src/ezaws"},
-    packages=["ezaws"],
+    packages=find_packages(exclude=("test*",)),
     description="An easy to use interface to AWS, ezaws.",
     url="https://github.com/saidvandeklundert/ezaws",
     classifiers=["Programming Language :: Python :: 3"],
